a781c25c9b43e8401e0f6a4fc4af214153c5f8cd
justin <justin@justindeMacBook-Air.local>
2020-01-16 22:52:00 +0800

    Initial commit

---
+++ Example/JGson/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  JGson
+//
+//  Created by zhoujabcd on 01/16/2020.
+//  Copyright (c) 2020 zhoujabcd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
